3c2f93847c0d172d7b0ba654cdb36eb46e48da84
PinkViolet <44185081+PinkViolet@users.noreply.github.com>
2019-03-15 09:47:04 -0400

    Add files via upload

--- pathfinding.py
+++ pathfinding.py
@@ -33,42 +33,74 @@ class Node:
         return self.pred
         
 
-row = 0 # total number of row
-col = 0 # total number of column
-S = () # Star point
-G = ()# End point
-grid = []
-input_file = "pathfinding_test.txt"
+rowA = 0 # total number of row
+colA = 0 # total number of column
+SA = () # Star point
+GA = ()# End point
+rowB = 0 # total number of row
+colB = 0 # total number of column
+SB = () # Star point
+GB = ()# End point
+gridA = []
+gridB = []
+input_file_a = "pathfinding_a.txt"
+input_file_b = "pathfinding_b.txt"
 output_file_a = "pathfinding_a_out.txt"
 output_file_b = "pathfinding_b_out.txt"
 
 # node = [(row, col), content, cost, pred]
 
 def getInputFile():
-    global grid
-    global row
-    global col
-    global input_file
-    global S
-    global G
+    global gridA
+    global rowA
+    global colA
+    global gridB
+    global rowB
+    global colB
+    global input_file_a
+    global input_file_b
+    global SA
+    global GA
+    global SB
+    global GB
     
-    infile = open(input_file, "r" )
-    grid = [[n for n in list(line)] for line in infile]
-    row = len(grid)
-    col = len(grid[row - 1])
-    for i in range(row):
-        for j in range(col):
+    infile = open(input_file_a, "r" )
+    gridA = [[n for n in list(line)] for line in infile]
+    rowA = len(gridA)
+    colA = len(gridA[rowA - 1])
+    for i in range(rowA):
+        for j in range(colA):
             n = Node()
             n.setPosition((i + 1, j + 1))
-            n.setContent(grid[i][j])
+            n.setContent(gridA[i][j])
             n.setCost(math.inf)
-            if grid[i][j] == 'S':
-                S = (i + 1, j + 1)
+            if gridA[i][j] == 'S':
+                SA = (i + 1, j + 1)
                 n.setCost(0)
                 n.setPred(-1)
-            elif grid[i][j] == 'G':
-                G = (i + 1, j + 1)
-            grid[i][j] = n
+            elif gridA[i][j] == 'G':
+                GA = (i + 1, j + 1)
+            gridA[i][j] = n
+
+    infile = open(input_file_b, "r" )
+    gridB = [[n for n in list(line)] for line in infile]
+    rowB = len(gridB)
+    colB = len(gridB[rowB - 1])
+    for i in range(rowB):
+        for j in range(colB):
+            n = Node()
+            n.setPosition((i + 1, j + 1))
+            n.setContent(gridB[i][j])
+            n.setCost(math.inf)
+            if gridB[i][j] == 'S':
+                SB = (i + 1, j + 1)
+                n.setCost(0)
+                n.setPred(-1)
+            elif gridB[i][j] == 'G':
+                GB = (i + 1, j + 1)
+            gridB[i][j] = n
+
+            
             
 def writeOutFile_A(pendingStr):
     global outputFile
@@ -80,12 +112,22 @@ def writeOutFile_B(pendingStr):
     with open(output_file_b, "a") as f:
         f.write(pendingStr + "\n")
 
-def pendingOutFile(grid, toReturn):
-    global row
-    global col
+def pendingOutFile_A(grid, toReturn):
+    global rowA
+    global colA
     
-    for i in range(row):
-        for j in range(col):
+    for i in range(rowA):
+        for j in range(colA):
+            toReturn += grid[i][j].getContent()
+        toReturn += '\n'
+    return toReturn
+
+def pendingOutFile_B(grid, toReturn):
+    global rowB
+    global colB
+    
+    for i in range(rowB):
+        for j in range(colB):
             toReturn += grid[i][j].getContent()
         toReturn += '\n'
     return toReturn
@@ -115,15 +157,15 @@ def find_Dneighbours(node, reached, grid):
 
 # Chebyshev heuristic
 def Dh(node):
-    global G
+    global GB
     position = node.getPosition()
-    return max(abs(G[0] - position[0]), abs(G[1] - position[1]))
+    return max(abs(GB[0] - position[0]), abs(GB[1] - position[1]))
 
 # Manhattan heuristic
 def Mh(node):
-    global G
+    global GA
     position = node.getPosition()
-    return abs(G[0] - position[0]) + abs(G[1] - position[1])
+    return abs(GA[0] - position[0]) + abs(GA[1] - position[1])
 
 def sort(queue):
     if len(queue) > 1:
@@ -158,15 +200,15 @@ def sort(queue):
     return queue
 
 def aStar_4():
-    global row
-    global col
-    global grid
-    global S
-    global G
+    global rowA
+    global colA
+    global gridA
+    global SA
+    global GA
 
-    result = grid
+    result = gridA
 
-    queue = [result[S[0] - 1][S[1] - 1]]
+    queue = [result[SA[0] - 1][SA[1] - 1]]
     reached = []
 
     while queue:
@@ -194,15 +236,15 @@ def aStar_4():
     return result
 
 def aStar_D():
-    global row
-    global col
-    global grid
-    global S
-    global G
+    global rowB
+    global colB
+    global gridB
+    global SB
+    global GB
 
-    result = grid
+    result = gridB
 
-    queue = [result[S[0] - 1][S[1] - 1]]
+    queue = [result[SB[0] - 1][SB[1] - 1]]
     reached = []
 
     while queue:
@@ -230,14 +272,14 @@ def aStar_D():
     return result
 
 def greedy_4():
-    global row
-    global col
-    global grid
-    global S
-    global G
+    global rowA
+    global colA
+    global gridA
+    global SA
+    global GA
 
-    result = grid
-    current_node = result[S[0] - 1][S[1] - 1]
+    result = gridA
+    current_node = result[SA[0] - 1][SA[1] - 1]
     reached = []
     FOUND = 0
 
@@ -261,14 +303,14 @@ def greedy_4():
     return result
 
 def greedy_D():
-    global row
-    global col
-    global grid
-    global S
-    global G
+    global rowB
+    global colB
+    global gridB
+    global SB
+    global GB
 
-    result = grid
-    current_node = result[S[0] - 1][S[1] - 1]
+    result = gridB
+    current_node = result[SB[0] - 1][SB[1] - 1]
     reached = []
     FOUND = 0
 
@@ -296,11 +338,16 @@ def greedy_D():
 
 
 def main():
-    global row
-    global col
-    global grid
-    global S
-    global G
+    global rowA
+    global colA
+    global gridA
+    global SA
+    global GA
+    global rowB
+    global colB
+    global gridB
+    global SB
+    global GB
 
     pendingOut_a = "Greedy\n"
     pendingOut_b = "Greedy\n"
@@ -308,17 +355,23 @@ def main():
     getInputFile()
 
     print("start Greedy")
-    pendingOut_a = pendingOutFile(greedy_4(), pendingOut_a)
-    pendingOut_b = pendingOutFile(greedy_D(), pendingOut_b)
+    pendingOut_a = pendingOutFile_A(greedy_4(), pendingOut_a)
+    pendingOut_b = pendingOutFile_B(greedy_D(), pendingOut_b)
     print("end Greedy")
 
+    getInputFile()
+    
     pendingOut_a += "\nA*\n"
     pendingOut_b += "\nA*\n"
     
     print("start A*")
-    pendingOut_a = pendingOutFile(aStar_4(), pendingOut_a)
-    pendingOut_b = pendingOutFile(aStar_D(), pendingOut_b)
+    pendingOut_a = pendingOutFile_A(aStar_4(), pendingOut_a)
+    pendingOut_b = pendingOutFile_B(aStar_D(), pendingOut_b)
     print("end A*")
+
+    #print(pendingOut_a)
+    #print(pendingOut_b)
+    
     writeOutFile_A(pendingOut_a)
     writeOutFile_B(pendingOut_b)
     
